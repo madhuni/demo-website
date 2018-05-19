cdf52a6e789dc927bfcc38b2b83cc4d2e43a8d64
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-05-19 15:31:02 +0530

    Initial commit
    Setting up the project

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        App is running fine!
+      </div>
+    );
+  }
+}
+
+export default App;
