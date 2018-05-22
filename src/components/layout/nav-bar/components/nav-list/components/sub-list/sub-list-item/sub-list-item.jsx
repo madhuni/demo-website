96da9d55df595638c2ced4e30436c807f55fe81b
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-05-22 19:35:02 +0530

    Add the click event on the sub-list-item
    Fix the styles for the component

--- src/components/layout/nav-bar/components/nav-list/components/sub-list/sub-list-item/sub-list-item.jsx
+++ src/components/layout/nav-bar/components/nav-list/components/sub-list/sub-list-item/sub-list-item.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './sub-list-item.css';
 
 const subListItem = (props) => {
   return (
-    <li className="sub-list-item">
-      <a href={props.link}>
+    <li className="sub-list-item" onClick={props.closeNavBar}>
+      <NavLink to={props.link}>
         <img src={props.url} alt="Item" className="item-image" width={props.width} height={props.height}/>
         <span className="sub-item-detail">
             {props.children}
         </span>
-      </a>
+      </NavLink>
     </li>
   );
 };
