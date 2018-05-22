b049abf7abc6bbe81abd516b018a6de7599f7569
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-05-22 13:34:02 +0530

    Add functionality to toggle nav-bar

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
+// Importing CSS for the App.jsx file
 import './app.css';
 
+// Importing Components used in App.jsx
+import Header from './components/layout/header/header';
+
 class App extends Component {
+  state = {
+    openNavBar: false
+  };
+
+  toggleNavBar = () => {
+    // console.log('hamburger is clicked');
+    if (this.state.openNavBar) {
+      this.setState({
+        openNavBar: false
+      });
+    } else {
+      this.setState({
+        openNavBar: true
+      });
+    }
+  }
+
   render() {
     return (
       <div className="app">
-        App is running fine!
+        <Header clicked={this.toggleNavBar} openNavBar={this.state.openNavBar} />
       </div>
     );
   }
