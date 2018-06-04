76dfc27009700f2e02a7b0933b72a17b26dd3dc6
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-06-04 11:43:46 +0530

    [Update] styles and structure for page-banner

--- src/components/ui/page-banner/page-banner.jsx
+++ src/components/ui/page-banner/page-banner.jsx
@@ -9,10 +9,11 @@ import './page-banner.css';
 
 const pageBanner = (props) => {
   return (
-    <div className="page-banner">
+    <div className={`page-banner ${props.classValue}`}>
       <div className="container container--page-banner">
         <h1 className="primary-heading u-text-center">
           <span className="primary-heading--main">{props.heading}</span>
+          <hr className="hr--primary-heading"/>
           <span className="primary-heading--sub">{props.subHeading}</span>
         </h1>
       </div>
