5ab035ecfa0b934c26e58a26b94392c386731736
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-06-20 15:53:40 +0530

    [Modify] section-heading component to have a sub-heading

--- src/components/pages-component/section-heading/section-heading.jsx
+++ src/components/pages-component/section-heading/section-heading.jsx
@@ -9,6 +9,11 @@ const sectionHeading = (props) => {
   return (
     <div className={props.classValue}>
       <h3 className="section-heading">{props.name}</h3>
+      {
+        props.hasSubHeading ?
+        <span className="section-sub-heading">{props.subHeading}</span> :
+        null
+      }
     </div>
   );
 };
