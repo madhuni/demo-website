2cc46dad0c3d609f420a76cc5af90f4a9dc3e235
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-06-29 13:46:44 +0530

    Update logo component

--- src/components/layout/logo/logo.jsx
+++ src/components/layout/logo/logo.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './logo.css';
 
 const logo = (props) => {
   return (
     <div className="logo-container flex-row" id={props.id}>
-      <img src={props.url} alt="Company Logo" className="logo" width={props.width} height={props.height}/>
+      <NavLink to='/home'>
+        <img src={props.url} alt="Company Logo" className="logo" width={props.width} height={props.height}/>
+      </NavLink>
     </div>
   );
 };
