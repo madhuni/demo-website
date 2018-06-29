c90ef0b7ea19e67cfcb62593f7f3e8f67a55cbda
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-06-29 13:37:32 +0530

    Update company-features component and sub-compnent
    Add new assets

--- src/pages/home/components/company-features/components/company-feature/company-feature.jsx
+++ src/pages/home/components/company-features/components/company-feature/company-feature.jsx
@@ -8,10 +8,14 @@ import React from 'react';
 import './company-feature.css';
 
 const companyFeature = (props) => {
+  const styles = {
+    backgroundImage: `linear-gradient(to left, rgba(255, 255, 255, 0.2), rgba(0, 0, 0, 0.2)), url(${props.url})`
+  };
+
   return (
     <div className="company-feature">
-      <div className="company-feature-img-container flex-row">
-        <img src={props.url} alt="Icon" width="100" height="100"/>
+      <div className="company-feature-img-container flex-row" style={styles}>
+        {/* <img src={props.url} alt="Icon" width="100" height="100"/> */}
       </div>
       <div className="company-feature-details">
         <div className="subtitle">
--- src/pages/home/components/company-features/features.js
+++ src/pages/home/components/company-features/features.js
@@ -3,22 +3,26 @@
  * __copyright__ = 'Copyright (C) 2018 Ethereal Machines Pvt. Ltd. All rights reserved'
  */
 
+import quality from './assets/images/quality.jpeg';
+import team from './assets/images/team.jpg';
+import vision from './assets/images/vision.jpeg';
+
 const features = [
   {
     id: 1,
-    iconUrl: 'https://via.placeholder.com/100x100',
+    iconUrl: quality,
     subtitle: 'Quality',
     text: 'Quality is our best policy. And we Live for the quality. We live to fulfill the dreams of million.'
   },
   {
     id: 2,
-    iconUrl: 'https://via.placeholder.com/100x100',
+    iconUrl: team,
     subtitle: 'Team',
     text: 'Team is our best policy. And we Live for the quality. We live to fulfill the dreams of million.'
   },
   {
     id: 3,
-    iconUrl: 'https://via.placeholder.com/100x100',
+    iconUrl: vision,
     subtitle: 'Vision',
     text: 'Vision is our best policy. And we Live for the quality. We live to fulfill the dreams of million.'
   }
