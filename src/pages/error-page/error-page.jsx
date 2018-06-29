03f3e84f9d52eb46f4add6773c39b604b59d604b
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-06-29 14:06:34 +0530

    Add error-page

--- src/pages/error-page/error-page.jsx
+++ src/pages/error-page/error-page.jsx
@@ -5,14 +5,29 @@
 
 import React, { Component } from 'react';
 
+import NavBtn from '../../components/ui/nav-btn/nav-btn';
+
 import './error-page.css';
 
 class ErrorPage extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
   render() {
     return (
       <div className="page page--error-page">
-        404
-        <p>Sorry! The page you are looking for doesn't exists.</p>
+        <section className="section section--404">
+          <div className="container">
+            <div className="content">
+              <h1 className="primary-heading u-text-center">
+                <span className="primary-heading--main">404</span>
+                <span className="primary-heading--sub">Oops, Sorry we can't find that page!</span>
+              </h1>
+              <NavBtn url={'home'} classValue={'btn--blue'}>Go to Home</NavBtn>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
