dca3823f54efa1ccf76532dacf2dcdc1d0fc8f54
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-06-29 13:44:14 +0530

    Update content-loader component

--- src/components/ui/content-loader/content-loader.jsx
+++ src/components/ui/content-loader/content-loader.jsx
@@ -7,13 +7,25 @@ import React from 'react';
 
 import './content-loader.css';
 
-import Spinner from '../spinner/spinner';
+// import Spinner from '../spinner/spinner';
 
 const contentLoader = (props) => {
   return (
-    <div className="content-loader">
-      <Spinner />
-      <p className="loading-text">Loading...</p>
+    // <div className="content-loader">
+    //   <Spinner />
+    //   <p className="loading-text">Loading...</p>
+    // </div>
+    <div className="content-loader content-loader--media flex-row">
+      <div className="img-loader-container"></div>
+      <div className="media-details-loader">
+        <div className="title-loader"></div>
+        <div className="publisher-date-loader">
+          <span className="publisher-loader"></span>
+          <span className="date-loader"></span>
+        </div>
+        <div className="description-loader"></div>
+        <div className="btn-loader"></div>
+      </div>
     </div>
   );
 };
